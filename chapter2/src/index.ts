0b82e6f6884f7bcce112c8b06114b4e1706025e6
Andy Chen <chenyangpoppin@hotmail.com>
2021-09-01 08:33:45 +0800

    feat: add a simple ts exercise

--- chapter2/src/index.ts
+++ chapter2/src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello TypeScript!');
+// console.log('Hello TypeScript!');
+
+// Exercise 1
+let a = 1 + 2;
+let b = a + 3;
+let c = {
+  apple: a,
+  banana: b
+};
+let d = c.apple * 4;
+
+console.log(d);
+
+// cannot add number with array
+// let e = a + [];
