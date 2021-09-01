77be32f85787dbd15c8e5f5e44de6f92bd817a22
Andy Chen <chenyangpoppin@hotmail.com>
2021-09-01 08:48:03 +0800

    chore: comment thing

--- chapter2/src/index.ts
+++ chapter2/src/index.ts
@@ -13,3 +13,12 @@ console.log(d);
 
 // cannot add number with array
 // let e = a + [];
+
+// ======================================================
+// REPL( Read - Eval[evaluate] - Print - Loop )
+// 1. 读取 - 执行 - 打印输出 - 循环
+// 2. 交互式解释器，就是你往REPL里输入一行代码，它立即给你执行结果
+// ======================================================
+
+// ts-node
+// ts-node is a TypeScript engine and REPL for Node.js, and it converts TypeScript into JavaScript
