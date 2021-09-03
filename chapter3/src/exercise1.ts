023b21aabbe807bf811461e795637165fd9e40f5
Andy Chen <chenyangpoppin@hotmail.com>
2021-09-03 09:18:26 +0800

    feat: add a little exercise regarding type unknown

--- chapter3/src/exercise1.ts
+++ chapter3/src/exercise1.ts
@@ -6,9 +6,19 @@ function squareOf(n: number) {
 squareOf(2);
 // squareOf('z'); // TS error, string cannot be assigned to number
 
-// 3.2
+// 3.2.1 any
+// we should avoid using any as much as possible
 let a: any = 666;            // any
 let b: any = ['danger'];     // any
 let c = a + b;               // any
 
 console.log(c);              // 666danger
+
+// 3.2.2 unknown
+// if you cannot predict a value's type, you should use unknown, dot not use any
+let d: unknown = 30;         // unknown
+let e = d === 123;           // boolean
+// let f = d + 10;              // error: Object is of type 'unkown'
+if (typeof d === 'number') {
+  let g = d + 10;            // number
+}
