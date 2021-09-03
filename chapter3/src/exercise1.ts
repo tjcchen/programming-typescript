79fb72cf824b20490be2b3ae3db9ab168d4366b6
Andy Chen <chenyangpoppin@hotmail.com>
2021-09-03 08:45:45 +0800

    feat: add a simple demo concerning type any

--- chapter3/src/exercise1.ts
+++ chapter3/src/exercise1.ts
@@ -5,3 +5,10 @@ function squareOf(n: number) {
 
 squareOf(2);
 // squareOf('z'); // TS error, string cannot be assigned to number
+
+// 3.2
+let a: any = 666;            // any
+let b: any = ['danger'];     // any
+let c = a + b;               // any
+
+console.log(c);              // 666danger
