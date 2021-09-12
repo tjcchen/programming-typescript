89e051c45d6b1c438ac2b6388531bcc439e85620
Andy Chen <chenyangpoppin@hotmail.com>
2021-09-12 22:16:16 +0800

    feat: add a demo exercise concerning bigint

--- chapter3/src/exercise1.ts
+++ chapter3/src/exercise1.ts
@@ -32,7 +32,7 @@ let ee: true = true;         // true, correct
 // let fff: true = false;    // Error: Type false is not assignable to type true
 
 
-// 3.2.4
+// 3.2.4 number
 let aaa = 1234;              // number
 let bbb = Infinity * 0.10;   // number
 const ccc = 5678;            // 5678
@@ -40,3 +40,14 @@ let ddd = aaa < bbb;         // boolean
 let eee: number = 100;       // number
 let fff: 26.218 = 26.218;    // 26.218
 // let ggg: 26.218 = 10;        // Error
+
+
+// 3.2.5 bigint( 使用需看是否支持 )
+let aaaa = 1234n;            // bigint
+let bbbb = 5678n;            // 5678n
+let cccc = aaaa + bbbb;      // bigint
+let dddd = aaaa < 1235;      // bigint
+let eeee = 88.5n;            // Error: a bigint literal must be an integer
+let ffff: bigint = 100n;     // bigint
+let gggg: 100n = 100n;       // 100n
+let hhhh: bigint = 100;      // Error: Type '100' is not assignable to type 'bigint'
