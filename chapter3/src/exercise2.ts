f0e67c8a63ca333e76e4565b8ef1fd8468bbbedd
Andy Chen <chenyangpoppin@hotmail.com>
2021-09-13 22:54:10 +0800

    bugfix: fix ts scope warning

--- chapter3/src/exercise2.ts
+++ chapter3/src/exercise2.ts
@@ -1,10 +1,10 @@
+export {}
+
 // 3.2.6 string
-if (true) {
-  let a = 'hello';          // string
-  // var b = 'billy';          // string
-  const c = '!';            // '!'
-  let d = a + ' ' + b + c;  // string
-  let e: string = 'zoom';   // string
-  let f: 'john' = 'john';   // john
-  // let g: 'john' = 'zoe';    // Error, Type zoe is not assignable to type john
-}
+let a = 'hello';          // string
+var b = 'billy';          // string
+const c = '!';            // '!'
+let d = a + ' ' + b + c;  // string
+let e: string = 'zoom';   // string
+let f: 'john' = 'john';   // john
+// let g: 'john' = 'zoe';    // Error, Type zoe is not assignable to type john
