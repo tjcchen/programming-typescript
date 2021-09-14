f99ead3b23cc82b0750d0f488987f9f2ab725c9c
Andy Chen <chenyangpoppin@hotmail.com>
2021-09-14 08:52:14 +0800

    feat: add symbol demo

--- chapter3/src/exercise2.ts
+++ chapter3/src/exercise2.ts
@@ -8,3 +8,11 @@ let d = a + ' ' + b + c;  // string
 let e: string = 'zoom';   // string
 let f: 'john' = 'john';   // john
 // let g: 'john' = 'zoe';    // Error, Type zoe is not assignable to type john
+
+// 3.2.7 symbol
+let aa = Symbol('a');            // symbol
+let bb: symbol = Symbol('b');    // symbol
+var cc = aa === bb;              // boolean, false
+// let dd = aa + 'x';               // Error, the '+' operator cannot be applied to type 'symbol'
+
+console.log(cc);
