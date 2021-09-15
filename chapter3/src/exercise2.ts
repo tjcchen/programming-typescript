78058cdd6aea55a583dbd6d91bce757634af8d9a
Andy Chen <chenyangpoppin@hotmail.com>
2021-09-15 22:57:53 +0800

    feat: add unique symbol demo

--- chapter3/src/exercise2.ts
+++ chapter3/src/exercise2.ts
@@ -15,4 +15,10 @@ let bb: symbol = Symbol('b');    // symbol
 var cc = aa === bb;              // boolean, false
 // let dd = aa + 'x';               // Error, the '+' operator cannot be applied to type 'symbol'
 
-console.log(cc);
+// unique symbol
+const ee = Symbol('e');                 // typeof e
+const ff: unique symbol = Symbol('f');  // typeof f
+// let gg: unique symbol = Symbol('f');    // Error: A variable whose type is 'unique symbol' type must be 'const'
+
+let hh = ee === ee;                      // boolean
+// let ii = ee === ff;                      // Error: This condition will always return 'false' since the types 'unique symbol' and 'unique symbol' have no overlap
