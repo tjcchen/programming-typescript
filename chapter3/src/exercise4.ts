f1a727ee63494b4977accfe3be234fdd8ad994d1
Andy Chen <chenyangpoppin@hotmail.com>
2021-09-16 22:46:02 +0800

    feat: one more extra object demo

--- chapter3/src/exercise4.ts
+++ chapter3/src/exercise4.ts
@@ -22,3 +22,11 @@ class Person {
 a = new Person('matt', 'smith'); // OK
 
 console.log(a.firstName, a.lastName); // matt, smith
+
+// (4)
+let aa: {bb: number};
+// aa = {};                // Error, Property 'b' is missing in type '{}' but required in type '{bb: number}'
+// aa = {
+//   bb: 1,
+//   cc: 2,                // Error, Object literal may only specify unknown properties, and 'cc' does not exist in type '{bb: number}'
+// }
