32a9824a919b381a9e52df954b97fe54dc56267a
Andy Chen <chenyangpoppin@hotmail.com>
2021-09-16 23:13:42 +0800

    feat: key can be any value

--- chapter3/src/exercise5.ts
+++ chapter3/src/exercise5.ts
@@ -2,6 +2,7 @@
 export {}
 
 // 3.2.8 optional properties
+// (1)
 let a: {
   b: number;
   c?: string;
@@ -15,3 +16,15 @@ a = {b: 1, c: 'd'};
 a = {b: 1, 10: true, 20: false};
 // a = {10: true};  // Error, b is missing
 // a = {b: 1, 33: 'red'}; // Error, property 33's value should be boolean
+
+// (2) key can be any value
+let airplaneSeatingAssignments: {
+  [seatNumber: string]: string
+} = {
+  '34D': 'Boris Cherny',
+  '34E': 'Bill Gates'
+};
+
+Object.keys(airplaneSeatingAssignments).forEach(key => {
+  console.log(key, airplaneSeatingAssignments[key]);
+});
