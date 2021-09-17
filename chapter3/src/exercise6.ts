6e56b45b08da00cefdd46018c0db571a8f8986b7
Andy Chen <chenyangpoppin@hotmail.com>
2021-09-17 09:14:21 +0800

    feat: please do dot use empty object

--- chapter3/src/exercise6.ts
+++ chapter3/src/exercise6.ts
@@ -1,6 +1,6 @@
 export {}
 
-// (1) identifier
+// (1) identifier (readonly, ?)
 let user: {
   readonly firstName: string
 } = {
@@ -10,3 +10,12 @@ let user: {
 console.log(user.firstName);
 
 // user.firstName = 'abbey with an e'; // Error, cannot assign to 'firstName' because it is a read-only property
+
+// (2) please do not use empty object
+let danger: {};
+danger = {};
+danger = {x: 1};
+danger = [];
+danger = 2;
+// danger = null;            // cannot assign null to empty object
+// danger = undefined;       // cannot assign undefined to empty object
