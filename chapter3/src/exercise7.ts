c16460cdc0586ce73dd76802eaed71a6f72a3dfd
Andy Chen <chenyangpoppin@hotmail.com>
2021-09-18 09:01:49 +0800

    feat: type alias cannot be declared twice

--- chapter3/src/exercise7.ts
+++ chapter3/src/exercise7.ts
@@ -24,3 +24,21 @@ let driver1: Person = {
 };
 
 console.log(driver1.name, driver1.age); // Kobe Bryant 60
+
+
+// (2) type alias cannot be declared twice, just like let const var
+type Color = 'red';
+// type Color = 'blue';    // Error: Duplicate identifer 'Color'
+
+let x = Math.random() < .5;
+if (x) {
+  type Color = 'blue';     // rewrite type Color = 'blue';
+  let b: Color = 'blue';
+  console.log(b);          // b is having its scope in if block
+} else {
+  let b: Color = 'red';
+  console.log(b);          // b is having its scope in else block
+}
+
+let c: Color = 'red';      // we now can only assign 'red' if we specify the type of c
+console.log(c);            // red
