5d665a0dc533d064d3b27e3894545bd672302ac2
Andy Chen <chenyangpoppin@hotmail.com>
2021-09-21 16:05:10 +0800

    feat: add readonly tuple and array

--- chapter3/src/exercise10.ts
+++ chapter3/src/exercise10.ts
@@ -36,4 +36,22 @@ let list: [number, boolean, ...string[]] = [1, false, 'a', 'b', 'c'];
 
 console.log(friends, list); // [ 'Jackie', 'Sara', 'Tali', 'Chloe', 'Claire' ] [ 1, false, 'a', 'b', 'c' ]
 
+// (3) readonly tuple and array
+// regular array is mutable, with push or splice, but sometimes we expect array can be immutable
+// we can operate .concat and .slice, but not .push and .splice
+let as: readonly number[] = [1, 2, 3];           // readonly number[]
+let bs: readonly number[] = as.concat(4);        // readonly number[]
+let three = bs[2];                               // number
+// as[4] = 5;  // Error: Index signature in type 'readonly number[]' only permits reading
+// as.push(6); // Error: Property 'push' does not exist on type 'readonly number[]'
+
+console.log(as, bs, three); // [ 1, 2, 3 ] [ 1, 2, 3, 4 ] 3
+
+// (4) readonly Array and Tuple grammar
+type A = readonly string[];         // readonly string[]
+type B = ReadonlyArray<string>;     // readonly string[]
+type C = Readonly<string[]>         // readonly string[]
+
+type D = readonly [number, string];  // readonly [number, string]
+type E = Readonly<[number, string]>; // readonly [number, string]
 
