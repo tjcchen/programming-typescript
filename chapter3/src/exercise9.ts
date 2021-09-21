bf30c5d7d07928488e6655539c66b614485c4928
Andy Chen <chenyangpoppin@hotmail.com>
2021-09-21 11:49:47 +0800

    feat: add two more examples concerning array

--- chapter3/src/exercise9.ts
+++ chapter3/src/exercise9.ts
@@ -22,3 +22,29 @@ g.push('red');                // (string | number)[]
 let h: number[] = [];
 h.push(1);
 // h.push('red');                // Error: Argument of type 'red' is not assignable to paramter of type 'number'
+
+
+// hybrid array
+// convert 'a' => 'A', 1 => 3
+let hybridArr = [1, 'a'];
+let retArr = hybridArr.map(_ => {
+  if (typeof _ === 'number') {
+    return _ * 3;
+  }
+  return _.toUpperCase();
+});
+
+console.log(retArr); // [3, 'A']
+
+// build array with different types
+function buildArray() {
+  let a = [];        // any[]
+  a.push(1);         // number[]
+  a.push('x');       // (string | number)[]
+  return a;
+}
+
+let myArray = buildArray();   // (string | number)[]
+// myArray.push(true);           // Error: Argument of type 'true' is not assignable to paramter of type 'string | number'
+
+console.log(myArray);   // [ 1, 'x' ]
