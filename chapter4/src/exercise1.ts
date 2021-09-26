9de6d270175c1d3a7f8c08f3e65f361f0eebe92c
Andy Chen <chenyangpoppin@hotmail.com>
2021-09-26 22:36:09 +0800

    feat: add function ts declaration

---
+++ chapter4/src/exercise1.ts
@@ -0,0 +1,18 @@
+// Chapter 4 Function
+// 1. declaration & invocation of function
+// 2. overloading
+// 3. polymorphism
+// 4. polymorphism alias
+
+// (1) implicitly annotation
+function add(a: number, b: number) {
+  return a + b;
+}
+
+// explicitly added annotation
+function add1(a: number, b: number): number {
+  return a + b;
+}
+
+
+
