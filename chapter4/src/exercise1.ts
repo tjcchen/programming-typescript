ab2dd4807114c105c9ebc38117ebce2950b2d955
Andy Chen <chenyangpoppin@hotmail.com>
2021-09-26 22:59:46 +0800

    feat: five ways of declaring functions

--- chapter4/src/exercise1.ts
+++ chapter4/src/exercise1.ts
@@ -14,5 +14,26 @@ function add1(a: number, b: number): number {
   return a + b;
 }
 
+// (2) five ways of declaring functions
+// 2.1 function name
+function greet(name: string) {
+  return 'hello ' + name; 
+}
 
+// 2.2 function expression
+let greet2 = function(name: string) {
+  return 'hello ' + name;
+};
 
+// 2.3 arrow function expression
+let greet3 = (name: string) => {
+  return 'hello ' + name;
+};
+
+// 2.4 arrow function shorthand
+let greet4 = (name: string) => 'hello ' + name;
+
+// 2.5 with Function
+let greet5 = new Function('name', 'return "hello " + name');
+
+console.log(greet5('jason')); // hello jason
