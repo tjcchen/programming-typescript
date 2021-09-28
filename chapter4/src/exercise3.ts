365a632e292bf5e4effdfeb253e86e4d7af91288
Andy Chen <chenyangpoppin@hotmail.com>
2021-09-28 22:53:34 +0800

    feat: rest parameter ts demo

--- chapter4/src/exercise3.ts
+++ chapter4/src/exercise3.ts
@@ -7,3 +7,19 @@ function sum(numbers: number[]): number {
 }
 
 console.log(sum([1, 2, 3])); // 6
+
+// (2) arguments
+function sumVariadic(): number {
+  return Array
+    .from(arguments)
+    .reduce((total, n) => total + n, 0);  // unsafe, total and n are 'any' type
+}
+
+// sumVariadic(1, 2, 4); // Error: this cannot work in ts environment
+
+// (3) correct way with rest params
+function sumVariadicSafe(...numbers: number[]): number {
+  return numbers.reduce((total, n) => total + n, 0);
+}
+
+console.log(sumVariadicSafe(4, 5, 6));   // 15
