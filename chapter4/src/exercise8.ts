b07cdccd9b6d6a28d1e7a0b28f53d37a34b586b0
Andy Chen <chenyangpoppin@hotmail.com>
2021-10-03 11:57:46 +0800

    feat: type level and value level

--- chapter4/src/exercise8.ts
+++ chapter4/src/exercise8.ts
@@ -1,6 +1,7 @@
 export {};
 
 // 4.1.7 invoke function signature
+// (1)
 type Add = (a: number, b: number) => number;
 
 type Greet = (name: string) => string;
@@ -21,3 +22,18 @@ let log: LogSafe = (
 };
 
 log('Andy'); // 2021-10-03T03:44:29.667Z Andy Not signed in
+
+
+// (2) type level and value level
+function area(radius: number): number | null {
+  if (radius < 0) {
+    return null;
+  }
+  return Math.PI * (radius ** 2);
+}
+
+let r: number = 3;
+let a = area(r);
+if (a !== null) {
+  console.info('result: ', a); // result:  28.274333882308138
+}
