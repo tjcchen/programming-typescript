85691951a94705cc758bbf290a7b4a9ca289d3f0
Andy Chen <chenyangpoppin@hotmail.com>
2021-10-07 20:42:03 +0800

    feat: add filter generic implementation

--- chapter4/src/exercise12.ts
+++ chapter4/src/exercise12.ts
@@ -14,3 +14,45 @@ function filter(array: any[], fn: Function) {
 // @ts-ignore
 const retVal = filter([1, 2, 3, 4], _ => _ < 3);
 console.log(retVal); // [1, 2]
+
+
+// (2) filter ts implementation
+type Filter = {
+  (array: unknown, fn: unknown): unknown[]
+}
+
+// not working
+type FilterSpecified = {
+  (array: number[], fn: (item: number) => boolean): number[]
+  (array: string[], fn: (item: string) => boolean): string[]
+  (array: object[], fn: (item: object) => boolean): object[]
+};
+
+// [perfect] working with generic
+type FilterGeneric = {
+  <T>(array: T[], fn: (item: T) => boolean): T[]
+};
+
+let filterGeneric: FilterGeneric = (array, fn) => {
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    let item = array[i];
+    if (fn(item)) {
+      result.push(item);
+    }
+  }
+  return result;
+};
+
+let names = [
+  { firstName: 'beth' },
+  { firstName: 'caitlyn' },
+  { firstName: 'xin' },
+];
+
+let result = filterGeneric(
+  names,
+  _ => _.firstName.startsWith('b')
+);
+
+console.log(result[0].firstName); // beth
