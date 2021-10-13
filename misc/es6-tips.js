5b121746f7bd54e0d5a660a00a255411f9ffbd7b
Andy Chen <chenyangpoppin@hotmail.com>
2021-10-13 23:01:46 +0800

    feat: add two more tips concerning es6

--- misc/es6-tips.js
+++ misc/es6-tips.js
@@ -44,4 +44,42 @@ const obj4 = {
 console.log(obj4); // { a: 1, b: 1 }
 
 
+// (3) concat string
+const name1 = 'james';
+const score = 59;
+const result = `${name1}${score > 60 ? ' passed': ' failed'}`;
+
+console.log(result); // james failed
+
+
+// (4) multiple if condition
+const type = 3;
+// if (
+//   type == 1 ||
+//   type == 2 ||
+//   type == 3 ||
+//   type == 4 ||
+// ) {
+//   // ...
+// }
+
+const condition = [1, 2, 3, 4];
+
+// true
+if (condition.includes(type)) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+
+// (5) replace filter with find, if only need to find value existance
+const arr4 = [1, 2, 3, 4, 5];
+const retVal = arr4.filter(x => x === 3);
+console.log(retVal); // [3]
+
+const retVal1 = arr4.find(x => x === 8);
+console.log(retVal1);  // undefined or 3
+
+
 
