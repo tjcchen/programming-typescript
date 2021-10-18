a016ca9448c7eb8477a0a002d63559483cdaab4d
Andy Chen <chenyangpoppin@hotmail.com>
2021-10-18 23:13:33 +0800

    feat: reservation reloading

--- chapter4/src/exercise20.ts
+++ chapter4/src/exercise20.ts
@@ -7,3 +7,37 @@ function map<T, U>(array: T[], fn: (item: T) => U): U[] {
   }
   return result;
 }
+
+// (T3) 
+type Reservation = unknown;
+
+type Reserve = {
+  (from: Date, to: Date, destination: string): Reservation
+  (from: Date, destination: string): Reservation
+  (destination: string): Reservation
+};
+
+let reserve: Reserve = (
+  fromOrDestination: Date | string,
+  toOrDestination?: Date | string,
+  destination?: string
+) => {
+  if (
+    fromOrDestination instanceof Date &&
+    toOrDestination instanceof Date &&
+    destination !== undefined
+  ) {
+    // Book a one-way trip
+  } else if (
+    fromOrDestination instanceof Date &&
+    typeof toOrDestination === 'string'
+  ) {
+    // Book a round trip
+  } else if (
+    typeof fromOrDestination === 'string'
+  ) {
+    // Book a trip right away
+  }
+ };
+
+reserve('beijing');
