58aee62ee90d71fc66262d5fd07e7565542a15b9
Andy Chen <chenyangpoppin@hotmail.com>
2021-10-20 08:39:50 +0800

    feat: add basic structure of Game

---
+++ chapter5/src/exercise1.ts
@@ -0,0 +1,19 @@
+// 5.1 Class and Interface
+// 1 round of chess game
+class Game {}
+
+// chess piece
+class Piece {}
+
+// position
+class Position {}
+
+//==========================
+// Chess Pieces
+//==========================
+class King extends Piece {}
+class Queen extends Piece {}
+class Biship extends Piece {}
+class Knight extends Piece {}
+class Rook extends Piece {}
+class Pawn extends Piece {}
