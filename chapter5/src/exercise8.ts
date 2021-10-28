cea169d155c08cebe732841aef392b19abf64dd5
Andy Chen <chenyangpoppin@hotmail.com>
2021-10-28 09:23:57 +0800

    feat: Class is structrual type

--- chapter5/src/exercise8.ts
+++ chapter5/src/exercise8.ts
@@ -11,3 +11,42 @@ export default null;
 // Abstract class can have a constructor providing a default implementation, and set modifier to properties
 // and methods. All of these traits cannot be implemented in interface.
 
+
+// 5.5 class is structrual type
+// (1) typescript compare whether two classes are the same, only compare their structure, not their class name
+class Zebra {
+  trot() {
+    // ...
+    console.log('zebra trot');
+  }
+}
+
+class Poodle {
+  trot() {
+    // ...
+    console.log('poodle trot');
+  }
+}
+
+function ambleAround(animal: Zebra) {
+  animal.trot();
+}
+
+// 使用 new 进行实例化，有没有括号都可以
+ambleAround(new Zebra()); // zebra trot
+ambleAround(new Poodle);  // poodle trot
+
+// (2) with modifier
+class A {
+  private x = 1;
+}
+
+class B extends A {
+}
+
+function fn(a: A) {}
+
+fn(new A);  // OK
+fn(new B);  // OK
+
+// fn({x: 1}); // Error: Property 'x' is private in type 'A' but not in type '{x: number}'
