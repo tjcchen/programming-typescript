4a0884c217444ab05433b59981d0513e469151b9
Andy Chen <chenyangpoppin@hotmail.com>
2021-10-28 09:01:55 +0800

    feat: uses abstract class or interface explanation

---
+++ chapter5/src/exercise8.ts
@@ -0,0 +1,13 @@
+export default null;
+
+// 5.4.3 use abstract class or interface
+// interface are more general and light weight, but the abstract class
+// has more functionality
+
+// (1) Interface is used to declare object structure, including object, array,
+// function, class etc. Interface does not generate js code, just persist during compile time
+
+// (2) Abstract class can build modal for classes, and it will generate js code in dist file, namely js class.
+// Abstract class can have a constructor providing a default implementation, and set modifier to properties
+// and methods. All of these traits cannot be implemented in interface.
+
