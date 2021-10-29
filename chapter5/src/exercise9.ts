5108241088c522843de8cf0a30262ca376e2fb3b
Andy Chen <chenyangpoppin@hotmail.com>
2021-10-29 09:13:58 +0800

    feat: add class and enum thing

--- chapter5/src/exercise9.ts
+++ chapter5/src/exercise9.ts
@@ -11,13 +11,23 @@ function b() {}
 
 // type
 type a = number;
-interface d {
+interface b {
   (): void
 };
 
 if (a + 1 > 3) {}  // a is value a, according to context
 let x: a = 9999;   // a is type a, according to context
 
+
 // (2) class and enum can not only generate type in type namespace,
 // but also value in value namespace
+class C {}
+let c: C  // instance type of class C
+= new C;  // value of class C
+
+enum E {F, G};
+let e: E  // enum type of Enum E
+= E.F;    // enum value of Enum E
+
+
 
