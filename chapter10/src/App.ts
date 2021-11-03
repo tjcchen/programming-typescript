3e8ba80fd1c186d143e5c0739fb2c2c3b09cfe57
Andy Chen <chenyangpoppin@hotmail.com>
2021-11-03 22:29:42 +0800

    feat: add nested namespaces

--- chapter10/src/App.ts
+++ chapter10/src/App.ts
@@ -2,4 +2,10 @@ namespace App {
   type GitRepo = {};
 
   Network.get<GitRepo>('https://api.github.com/repos/Microsoft/typescript');
+
+  Network.HTTP.get('GET');
+
+  Network.TCP.listenOn;
+
+  Network.Connection;
 }
--- chapter10/src/Get.ts
+++ chapter10/src/Get.ts
@@ -1,7 +1,31 @@
+// 1. A namespace can export function, variable, type, interface, or other namespaces.
+// 2. We can also have nested namespaces
+
 namespace Network {
+  export class Connection {}
+
   export function get<T>(url: string): Promise<T> {
-    return new Promise<T>((resolve, reject) => {
-      // ...
-    });
+    return new Promise<T>(() => {});
+  }
+
+  // nested namespaces
+  export namespace HTTP {
+    export function get<T>(url: string): Promise<T> {
+      return new Promise<T>(() => {});
+    }
+  }
+
+  export namespace TCP {
+    export function listenOn(port: number): Connection {
+      return new Connection;
+    }
+  }
+
+  export namespace UDP {
+    // ...
+  }
+
+  export namespace IP {
+    // ...
   }
 }
